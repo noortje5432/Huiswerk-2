e2f800482d4b0de40f8f28443d4afeca47d4eb41
sjjmlie <117653252+sjjmlie@users.noreply.github.com>
2023-06-11 11:48:19 +0200

    kleine verandering

--- evaluate_models.py
+++ evaluate_models.py
@@ -11,9 +11,9 @@ from nltk.corpus import conll2002 as conll
 testdata = conll.chunked_sents("ned.testa")
 
 #best.pickle
-other = pickle.load(open("best.pickle", "rb"))
-print("New_features performance:\n", other.accuracy(testdata))
+best = pickle.load(open("best.pickle", "rb"))
+print("New_features performance:\n", best.accuracy(testdata))
 
 #other.pickle
-best = pickle.load(open("other.pickle", "rb"))
-print("Other_new_features1 performance:\n", best.accuracy(testdata))
+other = pickle.load(open("other.pickle", "rb"))
+print("Other_new_features1 performance:\n", other.accuracy(testdata))
